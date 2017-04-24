21bba04a5fd0cbef77c55f5fd2c29c6da3730be5
Brian Bycynski <high5bri@hotmail.com>
2017-04-24 09:05:50 -0400

    added second grid example

--- src/components/CssGrid1.js
+++ src/components/CssGrid1.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import '../../src/styles/cssgrid1.scss'
 export default class CssGrid1 extends Component {
   render () {
-    return <div>
+    return <div className='flex1'>
       <div className='wrapper1'>
         <div className='box1 1a'>A</div>
         <div className='box1 1b'>B</div>
@@ -10,6 +10,14 @@ export default class CssGrid1 extends Component {
         <div className='box1 1d'>D</div>
         <div className='box1 1e'>E</div>
         <div className='box1 1f'>F</div>
+      </div>
+      <div className='wrapper2'>
+        <div className='box2 2a'>A</div>
+        <div className='box2 2b'>B</div>
+        <div className='box2 2c'>C</div>
+        <div className='box2 2d'>D</div>
+        <div className='box2 2e'>E</div>
+        <div className='box2 2f'>F</div>
       </div>
     </div>
   }
