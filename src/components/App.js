c62a8afcf0902baac6fb98dcff122cdf59d8208d
Brian Bycynski <high5bri@hotmail.com>
2017-04-24 08:09:04 -0400

    added first grid example

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,91 @@
 import React, { Component } from 'react'
-
-class App extends Component {
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import CssGrid1 from './CssGrid1'
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li className='list'><Link to='/'>Css Grid Example 1</Link></li>
+        <li><Link to='/ex2'>Css Grid Example 2</Link></li>
+        <li><Link to='/ex3'>Css Grid Example 3</Link></li>
+        <li><Link to='/ex4'>Css Grid Example 4</Link></li>
+        <li><Link to='/ex5'>Css Grid Example 5</Link></li>
+        <li><Link to='/ex6'>Css Grid Example 6</Link></li>
+        <li><Link to='/ex7'>Css Grid Example 7</Link></li>
+        <li><Link to='/ex8'>Css Grid Example 8</Link></li>
+        <li><Link to='/ex9'>Css Grid Example 9</Link></li>
+      </ul>
+      <hr />
+      <Route exact path='/' component={Ex1} />
+      <Route path='/ex2' component={Ex2} />
+      <Route path='/ex3' component={Ex3} />
+      <Route path='/ex4' component={Ex4} />
+      <Route path='/ex5' component={Ex5} />
+      <Route path='/ex6' component={Ex6} />
+      <Route path='/ex7' component={Ex7} />
+      <Route path='/ex8' component={Ex8} />
+      <Route path='/ex9' component={Ex9} />
+    </div>
+  </Router>
+)
+export default class App extends Component {
   render () {
     return <div>
-      <h1>Hello, World!</h1>
+      <BasicExample />
     </div>
   }
 }
 
-export default App
+const Ex1 = () => (
+  <div>
+    <h2><CssGrid1 /></h2>
+  </div>
+)
+
+const Ex2 = () => (
+  <div>
+    <h2>2</h2>
+  </div>
+)
+
+const Ex3 = () => (
+  <div>
+    <h2>3</h2>
+  </div>
+)
+
+const Ex4 = () => (
+  <div>
+    <h2>4</h2>
+  </div>
+)
+
+const Ex5 = () => (
+  <div>
+    <h2>5</h2>
+  </div>
+)
+
+const Ex6 = () => (
+  <div>
+    <h2>6</h2>
+  </div>
+)
+
+const Ex7 = () => (
+  <div>
+    <h2>7</h2>
+  </div>
+)
+
+const Ex8 = () => (
+  <div>
+    <h2>8</h2>
+  </div>
+)
+
+const Ex9 = () => (
+  <div>
+    <h2>9</h2>
+  </div>
+)
---
+++ src/components/CssGrid1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import '../../src/styles/cssgrid1.scss'
+export default class CssGrid1 extends Component {
+  render () {
+    return <div>
+      <div className='wrapper1'>
+        <div className='box1 1a'>A</div>
+        <div className='box1 1b'>B</div>
+        <div className='box1 1c'>C</div>
+        <div className='box1 1d'>D</div>
+        <div className='box1 1e'>E</div>
+        <div className='box1 1f'>F</div>
+      </div>
+    </div>
+  }
+}
